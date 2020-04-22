8e163b9deca8b3b83d36703cff3242b2500f4e7a
Rouge <code@r0uge.org>
2020-04-22 15:15:07 -0700

    Store different guilds' channels

--- Bot.py
+++ Bot.py
@@ -12,23 +12,28 @@ class BruhClient(discord.Client):
             message.content.startswith("!set")
             and message.author.server_permissions.administrator
         ):
-            d["channel-id"] = message.channel.id
-            await message.channel.send(
-                "Set channel to %s!" % message.channel.name
-            )
-            try:
-                with open("key.json", "w") as f:
-                    json.dump(d, f, indent=4)
-            except IOError as e:
-                print("Key.json went missing, yikes")
-                exit(self, 1)
+            guild = message.guild
+            if guild != None:
+                g_id = guild.id
+                key_name = "%s_channel" % g_id
+                d[key_name] = message.channel.id
+                await message.channel.send(
+                    "Set channel to %s!" % message.channel.name
+                )
+                try:
+                    with open("key.json", "w") as f:
+                        json.dump(d, f, indent=4)
+                except IOError as e:
+                    print("Key.json went missing, yikes")
+                    exit(self, 1)
         else:
             return
 
     async def on_member_remove(self, member):
         guild = member.guild
-        if "channel-id" in d:
-            channel = self.get_channel(d["channel-id"])
+        key_name = "%s_channel" % guild.id
+        if key_name in d:
+            channel = self.get_channel(d[key_name])
             msg = "bruh" if random.randint(0, 1) == 0 else "Bruh"
             await channel.send(msg)
 
