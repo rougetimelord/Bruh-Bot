13b538ed3dac01b6393c8b71806f574431956442
Rouge <code@r0uge.org>
2020-04-22 16:43:28 -0700

    Add on_join and on_remove listeners

--- Bot.py
+++ Bot.py
@@ -1,11 +1,31 @@
-import json, discord, random, asyncio
+import json, discord, random
 
 
 class BruhClient(discord.Client):
     async def on_ready(self):
         print("Logged in as %s, with ID %s" % (self.user.name, self.user.id))
 
+    async def on_guild_join(self, guild):
+        print("Joined guild: %s" % guild.name)
+        if guild.system_channel is not None:
+            await guild.system_channel.send(
+                "Hi! Thanks for adding BruhBot, to set the channel that I should send to send `!set` in that channel. Send `!test` to send a test message."
+            )
+
+    async def on_guild_remove(self, guild):
+        print("Removed from guild: %s" % guild.name)
+        key_name = "%s_channel" % guild.id
+        d.pop(key_name, None)
+        try:
+            print("Dumping leaving Guild: %s" % guild.name)
+            with open("key.json", "w") as f:
+                json.dump(d, f, indent=4)
+        except IOError as e:
+            print("Key.json went missing, yikes")
+            exit()
+
     async def on_message(self, message):
+        print("Message event dispatched")
         if message.author.id == self.user.id:
             return
         elif (
@@ -14,12 +34,11 @@ class BruhClient(discord.Client):
         ):
             guild = message.guild
             if guild != None:
-                g_id = guild.id
-                key_name = "%s_channel" % g_id
+                key_name = "%s_channel" % guild.id
                 if not key_name in d or d[key_name] != message.channel.id:
                     d[key_name] = message.channel.id
                     await message.channel.send(
-                        "Set channel to %s!" % message.channel.name
+                        "Set channel to `%s`!" % message.channel.name
                     )
                     try:
                         print(
