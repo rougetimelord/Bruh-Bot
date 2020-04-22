b7ed1c2e26b4d929b03ba955e3cc57ab612594e4
Rouge <code@r0uge.org>
2020-04-22 14:40:12 -0700

    Expand client
    Is this even correct? Why the fuck is the discord.py documentation such hot garbage.

--- Bot.py
+++ Bot.py
@@ -1,13 +1,14 @@
 import json, discord
-
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print("Logged in as %s" % client.user)
+from discord.ext import commands
 
 
+class BruhClient(discord.Client):
+    async def on_ready(self):
+        print("Logged in as %s" % client.user)
+
+
+d = ""
+client = discord.BruhClient()
 try:
     with open(key.json, "r") as f:
         d = json.load(f)
