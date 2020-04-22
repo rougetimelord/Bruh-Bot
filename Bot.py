71bd73efe0554f9f8c31d2b9438c3cad590c1b5e
Rouge <code@r0uge.org>
2020-04-22 15:00:06 -0700

    Add setup command

--- Bot.py
+++ Bot.py
@@ -1,13 +1,30 @@
 import json, discord
-from discord.ext import commands
 
 
 class BruhClient(discord.Client):
     async def on_ready(self):
-        print("Logged in as %s" % client.user)
+        print("Logged in as %s, with ID %s" % (self.name, self.user_id))
+
+    async def on_message(self, message):
+        if message.author.id == self.user_id:
+            return
+        elif (
+            message.content.startswith("!set")
+            and message.author.server_permissions.administrator
+        ):
+            d["channel-id"] = message.channel.id
+            await message.channel.send(
+                "Set channel to %s!" % message.channel.name
+            )
+        else:
+            return
+
+    async def on_member_remove(self, member):
+        guild = member.guild
+        if True:
+            pass
 
 
-d = ""
 client = discord.BruhClient()
 try:
     with open(key.json, "r") as f:
